0c9889baa0f3016e69ca12918458b10d45f25ef6
San Shwe <sanxhwe@gmail.com>
2024-03-19 15:12:00 +0630

    Products Section

--- src/app/_components/PageRange/index.tsx
+++ src/app/_components/PageRange/index.tsx
@@ -3,44 +3,42 @@ import React from 'react'
 import classes from './index.module.scss'
 
 const defaultLabels = {
-  plural: 'Docs',
   singular: 'Doc',
+  plural: 'Docs',
 }
 
 const defaultCollectionLabels = {
-  posts: {
-    plural: 'Products',
+  products: {
     singular: 'Product',
+    plural: 'Products',
   },
 }
 
 export const PageRange: React.FC<{
   className?: string
-  collection?: string
-  collectionLabels?: {
-    plural?: string
-    singular?: string
-  }
-  currentPage?: number
-  limit?: number
   totalDocs?: number
+  currentPage?: number
+  collection?: string
+  limit?: number
+  collectionLabels?: {
+    singular?: string
+    plural?: string
+  }
 }> = props => {
   const {
     className,
-    collection,
-    collectionLabels: collectionLabelsFromProps,
-    currentPage,
-    limit,
     totalDocs,
+    currentPage,
+    collection,
+    limit,
+    collectionLabels: collectionLabelsFromProps,
   } = props
 
-  let indexStart = (currentPage ? currentPage - 1 : 1) * (limit || 1) + 1
-  if (totalDocs && indexStart > totalDocs) indexStart = 0
-
+  const indexStart = (currentPage ? currentPage - 1 : 1) * (limit || 1) + 1
   let indexEnd = (currentPage || 1) * (limit || 1)
   if (totalDocs && indexEnd > totalDocs) indexEnd = totalDocs
 
-  const { plural, singular } =
+  const { singular, plural } =
     collectionLabelsFromProps || defaultCollectionLabels[collection || ''] || defaultLabels || {}
 
   return (
@@ -48,9 +46,7 @@ export const PageRange: React.FC<{
       {(typeof totalDocs === 'undefined' || totalDocs === 0) && 'Search produced no results.'}
       {typeof totalDocs !== 'undefined' &&
         totalDocs > 0 &&
-        `Showing ${indexStart}${indexStart > 0 ? ` - ${indexEnd}` : ''} of ${totalDocs} ${
-          totalDocs > 1 ? plural : singular
-        }`}
+        `Showing ${indexStart} - ${indexEnd} of ${totalDocs} ${totalDocs > 1 ? plural : singular}`}
     </div>
   )
 }
