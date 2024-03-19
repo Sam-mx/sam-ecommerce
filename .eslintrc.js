45f0d22fed16af05244d8116b0451845a1b29e89
San Shwe <sanxhwe@gmail.com>
2024-03-19 16:54:30 +0630

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,5 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
-  rules: {
-    "prettier/prettier":[
-      'error',{ endOfLine: 'auto'}
-    ],
-  },
+  
 }
